fe25e9ba56a5b64d88564fc47f3ba75f5595be95
Josh Reddick <josh.reddick@coxautoinc.com>
2019-01-30 14:23:44 -0500

    enable services by default.

--- src/bash/config_stream_camera.sh
+++ src/bash/config_stream_camera.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-v4l2-ctl --device /dev/streamcam -c exposure_auto_priority=0
+# v4l2-ctl --device /dev/streamcam -c exposure_auto_priority=0
 # v4l2-ctl --device /dev/streamcam -c focus_auto=0
 v4l2-ctl --device /dev/streamcam -c exposure_absolute=150
 v4l2-ctl --device /dev/streamcam -c exposure_auto=1
