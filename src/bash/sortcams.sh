11c2fd0ea4fa40f0da33d72ed8645ae2e8b1262e
Miriam Morris <jasminedragon6@gmail.com>
2019-02-26 21:17:32 -0500

    startup nice

--- src/bash/sortcams.sh
+++ src/bash/sortcams.sh
@@ -6,14 +6,18 @@
 declare -A CAM_MAP
 CAM_MAP[046d_0821_4804CA90]=visioncam  # logitech c910
 CAM_MAP[046d_HD_Pro_Webcam_C920_B778927F]=visioncam # c920
+CAM_MAP[046d_HD_Pro_Webcam_C920_7F11382F]=visioncam # c920
 CAM_MAP[HD_Camera_Manufacturer_USB_2.0_Camera]=streamcam  #fisheye.  no really.
 
 
 for cam in $(ls /dev/video*); do
-        echo camera: $cam
         #sudo udevadm info --query=all /dev/video0 | grep 'VENDOR_ID\|MODEL_ID\|SERIAL_SHORT'
         #cam_info=$(sudo udevadm info --query=all $cam | grep 'SERIAL_SHORT' | cut -d'=' -f2)
         cam_info=$(sudo udevadm info --query=all $cam | grep 'SERIAL=' | cut -d'=' -f2)
-        echo $cam_info
-        sudo ln -s $cam /dev/${CAM_MAP[$cam_info]}   
+        echo "camera $cam" 
+        if [[ -v CAM_MAP[$cam_info] ]]; then
+            echo "found $cam : $cam_info";
+            sudo rm /dev/${CAM_MAP[$cam_info]}
+            sudo ln -s $cam /dev/${CAM_MAP[$cam_info]}
+        fi        
 done
